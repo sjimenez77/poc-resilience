8e70d033272188efdb07b7463415da0d07741a3a
Santos Jiménez <santos.jimenez-linares@capgemini.com>
2018-01-17 13:22:50 +0100

    Angular Map and Material integrated with two styles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,253 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'app';
+  title = 'My first AGM project';
+  lat = 51.678418;
+  lng = 7.809007;
+  dark = true;
+  mapStyle = [
+    {
+      featureType: 'all',
+      elementType: 'labels.text.fill',
+      stylers: [
+        {
+          color: '#ffffff',
+        },
+      ],
+    },
+    {
+      featureType: 'all',
+      elementType: 'labels.text.stroke',
+      stylers: [
+        {
+          visibility: 'on',
+        },
+        {
+          color: '#3e606f',
+        },
+        {
+          weight: 2,
+        },
+        {
+          gamma: 0.84,
+        },
+      ],
+    },
+    {
+      featureType: 'all',
+      elementType: 'labels.icon',
+      stylers: [
+        {
+          visibility: 'off',
+        },
+      ],
+    },
+    {
+      featureType: 'administrative',
+      elementType: 'geometry',
+      stylers: [
+        {
+          weight: 0.6,
+        },
+        {
+          color: '#1a3541',
+        },
+      ],
+    },
+    {
+      featureType: 'landscape',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#2c5a71',
+        },
+      ],
+    },
+    {
+      featureType: 'poi',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#406d80',
+        },
+      ],
+    },
+    {
+      featureType: 'poi.park',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#2c5a71',
+        },
+      ],
+    },
+    {
+      featureType: 'road',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#29768a',
+        },
+        {
+          lightness: -37,
+        },
+      ],
+    },
+    {
+      featureType: 'transit',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#406d80',
+        },
+      ],
+    },
+    {
+      featureType: 'water',
+      elementType: 'geometry',
+      stylers: [
+        {
+          color: '#193341',
+        },
+      ],
+    },
+  ];
+
+  mapStyleB = [
+    {
+      featureType: 'administrative',
+      elementType: 'all',
+      stylers: [
+        {
+          saturation: '-100',
+        },
+      ],
+    },
+    {
+      featureType: 'administrative',
+      elementType: 'geometry',
+      stylers: [
+        {
+          visibility: 'simplified',
+        },
+      ],
+    },
+    {
+      featureType: 'administrative.province',
+      elementType: 'all',
+      stylers: [
+        {
+          visibility: 'off',
+        },
+      ],
+    },
+    {
+      featureType: 'landscape',
+      elementType: 'all',
+      stylers: [
+        {
+          saturation: -100,
+        },
+        {
+          lightness: 65,
+        },
+        {
+          visibility: 'on',
+        },
+      ],
+    },
+    {
+      featureType: 'poi',
+      elementType: 'all',
+      stylers: [
+        {
+          saturation: -100,
+        },
+        {
+          lightness: '50',
+        },
+        {
+          visibility: 'simplified',
+        },
+      ],
+    },
+    {
+      featureType: 'road',
+      elementType: 'all',
+      stylers: [
+        {
+          saturation: '-100',
+        },
+      ],
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'all',
+      stylers: [
+        {
+          visibility: 'simplified',
+        },
+      ],
+    },
+    {
+      featureType: 'road.arterial',
+      elementType: 'all',
+      stylers: [
+        {
+          lightness: '30',
+        },
+      ],
+    },
+    {
+      featureType: 'road.local',
+      elementType: 'all',
+      stylers: [
+        {
+          lightness: '40',
+        },
+      ],
+    },
+    {
+      featureType: 'transit',
+      elementType: 'all',
+      stylers: [
+        {
+          saturation: -100,
+        },
+        {
+          visibility: 'simplified',
+        },
+      ],
+    },
+    {
+      featureType: 'water',
+      elementType: 'geometry',
+      stylers: [
+        {
+          hue: '#ffff00',
+        },
+        {
+          lightness: -25,
+        },
+        {
+          saturation: -97,
+        },
+      ],
+    },
+    {
+      featureType: 'water',
+      elementType: 'labels',
+      stylers: [
+        {
+          lightness: -25,
+        },
+        {
+          saturation: -100,
+        },
+      ],
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
+// add these imports
+import { AgmCoreModule } from '@agm/core';
+import { AgmSnazzyInfoWindowModule } from '@agm/snazzy-info-window';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAsq4y4v6SBh9btsD8SjxlcG3RM8nFwpBo',
+    }),
+    AgmSnazzyInfoWindowModule,
+    MatButtonModule,
+    MatCheckboxModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
